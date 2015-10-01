46516e4a20e490140818c5acfd2ec4d3ebbdef6d
cen254@gmail.com <cen254@gmail.com>
2015-10-01 12:05:30 -0500

    Organization Chart Homework Assignment

---
+++ OrgChart/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrgChart
+{
+    class Employee
+    {
+        public int Name { get; set; }
+        private Employee _supervisor;
+        public Employee Supervisor {
+            get { } set; }
+        
+            
+        public IList<Employee> Minion { get; set; }
+
+        public 
+    }
+    class program
+    {
+        static void main(string[] args)
+        {
+        }
+    }
+}
